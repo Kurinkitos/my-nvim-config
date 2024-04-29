2c8c2cd9ab011d3c299f5fcfd0f469d638bf88b4
Kurinkitos <2537327+Kurinkitos@users.noreply.github.com>
2024-04-29 21:01:38 +0200

    Inital setup

---
+++ lua/keymaps.lua
@@ -0,0 +1,34 @@
+local keymap = vim.keymap -- for conciseness
+local wk = require("which-key")
+
+-- Clear highlights with leader + nh
+wk.register({
+    ["<leader>nh"] = {"<cmd>nohl<CR>", "clear highlights"},
+})
+
+-- Window mangement
+wk.register({
+    ["<leader>s"] = {
+        name = "+split",
+        v = {"<C-w>v", "split vertically"},
+        s = {"<C-w>s", "split horizontally"},
+        e = {"<C-w>=", "equal size"},
+        x = {"<cmd>close<CR>", "close current"},
+        -- Navigation
+        k = {"<cmd>wincmd k<CR>", "move up"},
+        j = {"<cmd>wincmd j<CR>", "move down"},
+        h = {"<cmd>wincmd h<CR>", "move left"},
+        l = {"<cmd>wincmd l<CR>", "move right"},
+    },
+})
+
+-- Tab manegment
+wk.register({
+    ["<leader>t"] = {
+        name = "+tab",
+        o = {"<cmd>tabnew<CR>", "new tab"},
+        x = {"<cmd>tabclose<CR>", "close tab"},
+        n = {"<cmd>tabn<CR>", "next tab"},
+        p = {"<cmd>tabp<CR>", "prev tab"},
+    },
+})
---
+++ lua/options.lua
@@ -0,0 +1,43 @@
+local opt = vim.opt -- for conciseness
+
+
+-- Line numbers
+opt.relativenumber = true
+opt.number = true
+
+-- Tabs and indentation
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.autoindent = true
+
+-- Disable line wrapping
+opt.wrap = false
+
+-- Search
+opt.ignorecase = true
+opt.smartcase = true
+
+
+-- Styling 
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+
+-- Backspace behavior
+-- Delete indents, delete char at eol, move to prev line at line start 
+opt.backspace = "indent,eol,start"
+
+-- Window splitting
+opt.splitright = true
+opt.splitbelow = true
+
+-- Use system clipboard by default
+opt.clipboard:append("unnamedplus")
+
+-- Make the which-key popup less eye melty
+vim.api.nvim_set_hl(0, 'FloatBorder', {bg='#3B4252', fg='#5E81AC'})
+vim.api.nvim_set_hl(0, 'NormalFloat', {bg='#3B4252'})
+vim.api.nvim_set_hl(0, 'TelescopeNormal', {bg='#3B4252'})
+vim.api.nvim_set_hl(0, 'TelescopeBorder', {bg='#3B4252'})
